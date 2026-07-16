da472b556b03f388201f1d5da8a6e34dcaad6de9
aditya-arch-7 <adityanyp20@gmail.com>
2026-07-16 15:25:02 +0530

    arrow adapt

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/matrixx_beryl.mk
+    $(LOCAL_DIR)/arrow_beryl.mk
 
 COMMON_LUNCH_CHOICES := \
-    matrixx_beryl-bp4a-userdebug \
-    matrixx_beryl-bp4a-user \
-    matrixx_beryl-bp4a-eng
+    arrow_beryl-bp4a-userdebug \
+    arrow_beryl-bp4a-user \
+    arrow_beryl-bp4a-eng
